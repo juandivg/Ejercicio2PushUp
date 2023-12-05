db4c82a220a09df8d7c764b6cbc844567adc65cc
juandivg <117705664+juandivg@users.noreply.github.com>
2023-12-04 21:48:13 -0500

    Add files via upload

---
+++ Ejercicio2PushUp/Ejercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine("Ingrese el multiplicador");
+
+int multiplicador= int.Parse(Console.ReadLine());
+System.Console.WriteLine("Ingrese el multiplicando");
+int multiplicando= int.Parse(Console.ReadLine());
+
+
+int sum=0;
+
+
+while(multiplicador>=1)
+{
+    if(multiplicador%2==1){
+        sum=sum+multiplicando;
+    }
+    multiplicando*=2;
+    multiplicador/=2;
+
+}
+
+System.Console.WriteLine("El resultado es "+ sum);
